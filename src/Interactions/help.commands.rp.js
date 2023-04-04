8e1009330fb6a1de0b49c7b88a206a116a465030
Aninha Pardini <aninhapardini.contato@gmail.com>
2023-04-04 14:32:23 -0300

    att: use switch

--- src/Interactions/help.commands.rp.js
+++ src/Interactions/help.commands.rp.js
@@ -6,69 +6,47 @@ const commandsReply = async (interaction) => {
   const selected = interaction.isStringSelectMenu()
     ? interaction.values[0]
     : false;
-  if (selected === INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_OPTIONS.ASKCREATE) {
-    const embed = new EmbedBuilder()
-      .setColor(COLORS.EMBEDCOLOR_GRAY)
-      .setTitle('``✏`` Como criar um bolão?')
-      .setDescription(
-        'Para criar uma liga é só digitar ``/setup``\n\nCom isso, é só clicar em continuar e você será enviado para a configuração da liga.',
-      );
-    const components = new ActionRowBuilder().setComponents(
-      new ButtonBuilder()
-        .setCustomId(INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_SELECT)
-        .setLabel('Voltar')
-        .setStyle(ButtonStyle.Secondary),
-    );
 
-    await interaction.update({
-      ephemeral: true,
-      embeds: [embed],
-      components: [components],
-    });
-  } else if (
-    selected === INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_OPTIONS.ASKADM
-  ) {
-    const embed = new EmbedBuilder()
-      .setColor(COLORS.EMBEDCOLOR_GRAY)
-      .setTitle(
-        '``👤`` Como definir ADM para o bolão? ``FUNCIONALIDADE EM MANUTENÇÃO``',
-      )
-      .setDescription(
-        'Para adicionar um moderador a liga é só digitar ``/settings`` e para remover basta clicar em ``Moderação``',
-      );
-    const components = new ActionRowBuilder().setComponents(
-      new ButtonBuilder()
-        .setCustomId(INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_SELECT)
-        .setLabel('Voltar')
-        .setStyle(ButtonStyle.Secondary),
-    );
+  const embed = new EmbedBuilder().setColor(COLORS.EMBEDCOLOR_GRAY);
+  const components = new ActionRowBuilder().setComponents(
+    new ButtonBuilder()
+      .setCustomId(INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_SELECT)
+      .setLabel('Voltar')
+      .setStyle(ButtonStyle.Secondary),
+  );
 
-    await interaction.update({
-      ephemeral: true,
-      embeds: [embed],
-      components: [components],
-    });
-  } else if (
-    selected === INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_OPTIONS.ASKUPRANK
-  ) {
-    const embed = new EmbedBuilder()
-      .setColor(COLORS.EMBEDCOLOR_GRAY)
-      .setTitle('``🔄`` Como atualizar o ranking?')
-      .setDescription(
-        'Para atualizar os jogos, resultados e ranking, é só digitar ``/admin update channels``',
-      );
-    const components = new ActionRowBuilder().setComponents(
-      new ButtonBuilder()
-        .setCustomId(INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_SELECT)
-        .setLabel('Voltar')
-        .setStyle(ButtonStyle.Secondary),
-    );
-
-    await interaction.update({
-      ephemeral: true,
-      embeds: [embed],
-      components: [components],
-    });
+  switch (selected) {
+    case INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_OPTIONS.ASKCREATE:
+      embed
+        .setTitle('``✏`` Como criar um bolão?')
+        .setDescription(
+          'Para criar uma liga é só digitar ``/setup``\n\nCom isso, é só clicar em continuar e você será enviado para a configuração da liga.',
+        );
+    case INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_OPTIONS.ASKADM:
+      embed
+        .setTitle(
+          '``👤`` Como definir ADM para o bolão? ``FUNCIONALIDADE EM MANUTENÇÃO``',
+        )
+        .setDescription(
+          'Para adicionar um moderador a liga é só digitar ``/settings`` e para remover basta clicar em ``Moderação``',
+        );
+    case INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_OPTIONS.ASKUPRANK:
+      embed
+        .setTitle('``🔄`` Como atualizar o ranking?')
+        .setDescription(
+          'Para atualizar os jogos, resultados e ranking, é só digitar ``/admin update channels``',
+        );
+    case INTERACTION_IDS.HELP_SELECTMENU.COMMANDS_OPTIONS.ASKLANG:
+      embed
+        .setTitle('``🌎`` Como mudar o idioma?')
+        .setDescription(
+          'Para mudar o idioma basta ir em ``/settings`` e em **editar preferências** escolher **idioma**.',
+        );
   }
+  await interaction.update({
+    ephemeral: true,
+    embeds: [embed],
+    components: [components],
+  });
 };
 module.exports = { commandsReply };
